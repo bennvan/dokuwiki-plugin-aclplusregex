94af4dc4ab99fe403ef9f1c8a95bc8763a22cef2
Andreas Gohr <andi@splitbrain.org>
2020-05-04 12:28:18 +0200

    added settings description

---
+++ lang/en/settings.php
@@ -0,0 +1,3 @@
+<?php
+
+$lang['run'] = 'When to apply this plugin\'s ACL rules? Rules applied AFTER DokuWiki\'s ACL handling will only grant additional permission if they are higher. Rules applied BEFORE will abort any further ACL processing by DokuWiki when a matching rule has been found.';
